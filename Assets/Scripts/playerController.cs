0f03e465dacb38453250a83fc2ef239b7672b9da
Pablo <pablo.0698@hotmail.com>
2020-12-12 17:45:37 -0500

    first commit

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    private Animator _animController;
+    public AnimationClip _iddle, _jump;
+
+    private void Awake()
+    {
+        _animController = GetComponent<Animator>();
+    }
+}
